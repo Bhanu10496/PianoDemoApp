e52b10268d5fd6c6b4916231bf58e34223bcef85
bhanu <bhanu10496@gmail.com>
2017-03-06 23:30:10 +0530

    cleaned the code

--- app/src/main/java/com/example/android/pianodemo/MainActivity.java
+++ app/src/main/java/com/example/android/pianodemo/MainActivity.java
@@ -10,11 +10,11 @@ import android.widget.Button;
 import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
-    static int loop = 1;
+    static int loop = 1,x=0;
     MediaPlayer m;
-    static int x=0;
     int flag = 0;
-    static Button b,bs;
+    static Button b, bs;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -34,7 +34,6 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 flag = 0;
                 final String s = e.getText().toString();
-//                new helper().execute(s);
                 new Thread(new Runnable() {
                     @Override
                     public void run() {
@@ -48,27 +47,9 @@ public class MainActivity extends AppCompatActivity {
                                 m = MediaPlayer.create(MainActivity.this, R.raw.a1);
                                 m.setAudioStreamType(AudioManager.STREAM_MUSIC);
                                 m.start();
-                                m.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
-                                    @Override
-                                    public void onCompletion(MediaPlayer mp) {
-                                        //   mp.release();
-                                        //Toast.makeText(getApplicationContext(),"complete", Toast.LENGTH_SHORT).show();
-                                        //Log.e("main","on completion called");
-                                        //loop=0;
-                                    }
-                                });
                                 while (m.isPlaying()) {
-//                        try {
-//                            Thread.sleep(50);
-//                        }
-//                        catch(Exception e)
-//                        {
-//                            Log.e("main","error");
-//                        }
-                                    //Log.e("main","while loop 1");
-                                    //do nothing
                                 }
-                                //loop=1;
+
                             } else if ((s.charAt(i) == 'b' || s.charAt(i) == 'B') && s.charAt(i + 1) == '1') {
                                 i++;
                                 m = MediaPlayer.create(MainActivity.this, R.raw.b1);
@@ -159,108 +140,15 @@ public class MainActivity extends AppCompatActivity {
                                 } catch (Exception e) {
                                     Log.e("main", "error");
                                 }
-                                //Log.e("main","while loop 2");
-                                //do nothing
-
-                                //loop=1;
                             }
-                            if (i == l - 1)
-                            {
+                            if (i == l - 1) {
                                 m.release();
-                                x=1;
+                                x = 1;
                             }
                         }
                     }
                 }).start();
-
             }
         });
     }
 }
-//class helper extends AsyncTask<String,Void,Void>
-//{
-//    @Override
-//    protected Void doInBackground(String... params) {
-//        int l=params[0].length();
-//        for(int i=0;i<l;i++)
-//        {
-//            if(params[0].charAt(i)=='a')
-//            {
-//                MediaPlayer m = MediaPlayer.create(MainActivity.this, R.raw.a1);
-//                m.start();
-//                m.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
-//                    @Override
-//                    public void onCompletion(MediaPlayer mp) {
-//                        mp.release();
-//                        //Toast.makeText(getApplicationContext(),"complete", Toast.LENGTH_SHORT).show();
-//                        //Log.e("main","on completion called");
-//                        //loop=0;
-//                    }
-//                });
-//                while(m.isPlaying())
-//                {
-////                        try {
-////                            Thread.sleep(50);
-////                        }
-////                        catch(Exception e)
-////                        {
-////                            Log.e("main","error");
-////                        }
-//                    //Log.e("main","while loop 1");
-//                    //do nothing
-//                }
-//                //loop=1;
-//            }
-//            else if(params[0].charAt(i)=='b')
-//            {
-//                MediaPlayer m = MediaPlayer.create(MainActivity.this, R.raw.a1);
-//                m.start();
-//                m.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
-//                    @Override
-//                    public void onCompletion(MediaPlayer mp) {
-//                        mp.release();
-//                    //    Toast.makeText(getApplicationContext(),"complete", Toast.LENGTH_SHORT).show();
-//                        //loop=0;
-//                    }
-//                });
-//                while(m.isPlaying())
-//                {
-////                            try {
-////                                Thread.sleep(50);
-////                            }
-////                            catch(Exception e)
-////                            {
-////                                Log.e("main","error");
-////                            }
-//                    //Log.e("main","while loop 2");
-//                    //do nothing
-//                }
-//                //loop=1;
-//            }
-//            else if(params[0].charAt(i)=='.')
-//            {
-//                            try {
-//                                Thread.sleep(50);
-//                            }
-//                            catch(Exception e)
-//                            {
-//                                Log.e("main","error");
-//                            }
-//                    //Log.e("main","while loop 2");
-//                    //do nothing
-//
-//                //loop=1;
-//            }
-//        }
-//    return null;
-//    }
-//}
-//}
-
-
-
-
-
-
-
-
